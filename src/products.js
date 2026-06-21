a65a8838854dd164329d744f737bfa0b49729324
Ramanathan06 <ramanathan06ai@gmail.com>
2026-06-21 23:27:47 +0530

    images uploaded2

--- src/products.js
+++ src/products.js
@@ -1,12 +1,12 @@
   export const products = [
     { id: 1, name: "Wireless Headphones", category:
   "Electronics", price: 99, image:
-  "/images/headphones.jpeg" },
+  "/images/Wireless headphones.jpeg" },
     { id: 2, name: "Running Shoes", category:
-  "Footwear", price: 59, image: "/images/shoes.jpeg"
+  "Footwear", price: 59, image: "/images/Running shoes.jpeg"
   },
     { id: 3, name: "Coffee Mug", category: "Kitchen",
-  price: 12, image: "/images/mug.jpeg" },
+  price: 12, image: "/images/coffee mug.jpeg" },
     { id: 4, name: "Backpack", category:
   "Accessories", price: 45, image:
   "/images/backpack.jpeg" },
